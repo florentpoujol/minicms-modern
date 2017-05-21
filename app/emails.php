6467dc8c5d1d835a1deefba564a36f9f57c7354b
Florent Poujol <florent.poujol@gmail.com>
2017-05-21 13:13:41 +0200

    Allow to send email via local software or SMTP. Store email content in languages strings.

--- app/emails.php
+++ app/emails.php
@@ -6,39 +6,52 @@ class Emails
 {
     public static function send($to, $subject, $body)
     {
-        global $smtpHost, $smtpUser, $smtpPassword, $emailFrom;
-        $mail = new PHPmailer;
+        $formAddress = Config::get("mailer_from_address");
+        $formName = Config::get("mailer_from_name");
 
-        $mail->isSMTP();
-        // $mail->SMTPDebug = 3;
-        $mail->Host = $smtpHost;
-        $mail->SMTPAuth = true;
-        $mail->Username = $smtpUser;
-        $mail->Password = $smtpPassword;
-        $mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;
+        if (Config::get("smtp_host", "") === "") {
+            $headers = "MIME-Version: 1.0 \n";
+            $headers .= "Content-type: text/html; charset=utf-8 \n";
+            $headers .= "From: $formAddress <$formName> \n";
+            $headers .= "Reply-To: $formAddress \n";
 
-        $mail->setFrom($emailFrom, 'MimiCMS MVC Mailer');
-        $mail->addAddress($to);
-        $mail->isHTML(true);
+            if (! mail($to, $subject, $body, $headers)) {
+                Messages::addError("email.notsent");
+                return false;
+            }
+        } else {
+            $mail = new \PHPmailer;
 
-        $mail->Subject = $subject;
-        $mail->Body    = $body;
-        $mail->AltBody = strip_tags($body);
+            $mail->isSMTP();
+            // $mail->SMTPDebug = 3;
+            $mail->Host = Config::get("smtp_host");
+            $mail->SMTPAuth = true;
+            $mail->Username = Config::get("smtp_user");
+            $mail->Password = Config::get("smtp_password");
+            $mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;
 
-        if($mail->send() === false) {
-            Messages::addError("Email wasn't sent !");
-            Messages::addError("Mailer Error: ".$mail->ErrorInfo);
-            return false;
+            $mail->setFrom($formAddress, $formName);
+            $mail->addAddress($to);
+            $mail->isHTML(true);
+
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->AltBody = strip_tags($body);
+
+            if (! $mail->send()) {
+                Messages::addError("email.notsent");
+                Messages::addError("Mailer Error: " . $mail->ErrorInfo);
+                return false;
+            }
         }
-
         return true;
     }
 
     public static function sendConfirmEmail($user)
     {
-        $subject = "Confirm your email address";
-        $body = "You have registered or changed your email address on the site. <br> Please click the link below to verify the email adress. <br><br>";
+        $subject = Lang::get("email.confirmemail.subject");
+        $body = Lang::get("email.confirmemail.body");
         $link = App::$url."index.php?c=register&a=confirmemail&id=".$user->id."&token=".$user->email_token;
         $body .= "<a href='$link'>$link</a>";
 
@@ -47,8 +60,8 @@ class Emails
 
     public static function sendChangePassword($user)
     {
-        $subject = "Change your password";
-        $body = "You have requested to change your password. <br> Click the link below within 48 hours to access the form.<br>";
+        $subject = Lang::get("email.changepassword.subject");
+        $body = Lang::get("email.changepassword.subject");
         $link = App::$url."index.php?c=login&a=resetpassword&id=".$user->id."&token=".$user->password_token;
         $body .= "<a href='$link'>$link</a>";
 
